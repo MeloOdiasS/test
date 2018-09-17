d0292a43277cb55beee25dd3d2fb3b018b23212e
MeloOdiasS <gt@securite-gps.fr>
2018-09-17 10:51:37 +0200

    Changes to be committed: 	new file:   index.php  Changes not staged for commit: 	modified:   README.md

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php session_start();?>
+<!doctype html>
+<html lang="fr">
+<head>
+	<title>L'aile et la Cuisse</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="verify-v1" content="/wjjKcosAHmTYUcM8nIP5dvZ2Xruyn/5yOXCZbD9aCU=">
+	<meta name="author" content="Tristram Geoffrey">
+	<link rel="icon" type="image/png" href="img/favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+</head>
+<header>
+</header>
+<main>
+</main>
+</body>
